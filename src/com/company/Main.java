122caddad8e4020a98cf48199d568e3fc9da53e2
Famy9 <matas.lataitis@gmail.com>
2017-12-01 19:49:47 +0200

    Septinta Uzduotis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Iveskite kiek skaiciu norite ivesti: ");
+        Scanner skaneris = new Scanner(System.in);
+        int a = skaneris.nextInt();
+        int[] Skaiciai = new int[a];
+
+        for (int i = 0; i < a; i++) {
+            System.out.println("Iveskite Skaiciu:"+ (i + 1));
+            int skaicius = skaneris.nextInt();
+            Skaiciai[i] = skaicius;
+
+        }
+        System.out.println("Skaiciai virs simto:");
+        for (int sk = 0; sk < a; sk++) {
+            if (Skaiciai[sk] > 100) {
+                System.out.println("Skaiciai virs 100: " + Skaiciai[sk]);
+            }
+
+        }
     }
 }
+
